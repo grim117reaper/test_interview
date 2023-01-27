f3c5f0267dbea85f629d1debb0caad2c4b1e8a5c
Aman Saxena <aman.a.saxena@ericsson.com>
2023-01-27 19:39:36 +0530

    question1-2nd itteration

--- question1/test1.py
+++ question1/test1.py
@@ -3,11 +3,19 @@ s1 = "amansaxena"
 s2 = "ytyasaxenaman"
 
 def longest_substring(s1,s2):
-    for max_length in range(len(s2),0,-1):
-        starting_positions = [x for x in range(0,len(s2)-max_length+1)]
-        substrings = [s2[x:x+max_length] for x in starting_positions]
+
+    if len(s1) >= len(s2):
+        longer_string = s1
+        shorter_string = s2
+    else:
+         shorter_string = s1
+         longer_string = s2
+         
+    for max_length in range(len(shorter_string),0,-1):
+        starting_positions = [x for x in range(0,len(shorter_string)-max_length+1)]
+        substrings = [shorter_string[x:x+max_length] for x in starting_positions]
         for x in substrings:
-            if x in s1:
+            if x in longer_string:
                 return(x)
 
 print(longest_substring(s1,s2))
